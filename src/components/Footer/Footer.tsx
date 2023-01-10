047f3f52ea26ddc3b6e6a14c0ef70cecc521ec9f
Rafael Fernandes Lopes <rfernandes@fullstacklabs.co>
2023-01-10 15:43:36 -0300

    first commit

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,15 @@
+import "./Footer.scss";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-12">Branding info</div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/layouts/MainLayout/MainLayout.tsx
@@ -0,0 +1,23 @@
+import { ReactNode } from "react";
+import Footer from "../../components/Footer/Footer";
+import Header from "../../components/Header/Header";
+
+import "./MainLayout.scss";
+
+interface MainLayoutProps {
+  children?: ReactNode;
+}
+
+const MainLayout = ({ children }: MainLayoutProps) => {
+  return (
+    <div className="main-layout">
+      <Header />
+      <main>
+        <div className="container-fluid">{children}</div>
+      </main>
+      <Footer />
+    </div>
+  );
+};
+
+export default MainLayout;
